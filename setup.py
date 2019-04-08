45bac34b28a5e739219968e93cd1bec8ea76414c
SwathiKuchukulla <40610603+SwathiKuchukulla@users.noreply.github.com>
2019-04-08 14:38:13 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
     include_package_data=True,
     install_requires=[
         'flask',
-        'rascore@git+https://github.com/SwathiKuchukulla/ra_score.git',
+        'Risk-Adjustment-Scoring-Engine@git+https://github.com/SwathiKuchukulla/ra_score.git',
         'jsonschema',
         'httpie',
         'ldap3'
